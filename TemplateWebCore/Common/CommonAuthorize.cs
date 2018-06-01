3936a766a04426a82885d1ef1c899a0a98ca69e0
minhquan221 <minhquan221@users.noreply.github.com>
2018-06-01 13:17:00 +0700

    commit
    commit

---
+++ TemplateWebCore/Common/CommonAuthorize.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TemplateWebCore.Common
+{
+    public class CommonAuthorize : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase contextBase)
+        {
+            return base.AuthorizeCore(contextBase);
+        }
+    }
+}
